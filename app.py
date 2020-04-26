245025f20ed979c4aac7b89621d65d3f3be5d081
FKeir <fmkeir@gmail.com>
2020-04-26 21:32:54 +0100

    Refactor Product model out of app

--- app.py
+++ app.py
@@ -1,7 +1,9 @@
 from flask import Flask, request, jsonify
-from flask_sqlalchemy import SQLAlchemy
+from db import db
 from flask_marshmallow import Marshmallow
 import os
+
+from models.product import Product
 
 app = Flask(__name__)
 basedir = os.path.abspath(os.path.dirname(__file__))
@@ -9,21 +11,7 @@ basedir = os.path.abspath(os.path.dirname(__file__))
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///' + os.path.join(basedir, 'db.sqlite')
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 
-db = SQLAlchemy(app)
 ma = Marshmallow(app)
-
-class Product(db.Model):
-    id = db.Column(db.Integer, primary_key=True)
-    name = db.Column(db.String(100), unique=True)
-    description = db.Column(db.String(200))
-    price = db.Column(db.Float)
-    quantity = db.Column(db.Integer)
-
-    def __init__(self, name, description, price, quantity):
-        self.name = name
-        self.description = description
-        self.price = price
-        self.quantity = quantity
 
 class ProductSchema(ma.Schema):
     class Meta:
@@ -87,4 +75,5 @@ def get():
     return jsonify({'msg': 'hello world!'})
 
 if __name__ == '__main__':
+    db.init_app(app)
     app.run(debug=True)
