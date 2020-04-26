047225055090110ee21ce4d658d35e6e6463497c
FKeir <fmkeir@gmail.com>
2020-04-26 22:36:33 +0100

    Refactor product routes to separate controller

--- app.py
+++ app.py
@@ -1,9 +1,6 @@
-from flask import Flask, request, jsonify
+from flask import Flask, jsonify
 import os
-
 from db import db
-from models.product import Product
-from schemas.productSchema import ProductSchema, product_schema, products_schema
 
 app = Flask(__name__)
 basedir = os.path.abspath(os.path.dirname(__file__))
@@ -11,55 +8,9 @@ basedir = os.path.abspath(os.path.dirname(__file__))
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///' + os.path.join(basedir, 'db.sqlite')
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 
-@app.route('/products', methods=['POST'])
-def add_product():
-    name = request.json['name']
-    description = request.json['description']
-    price = request.json['price']
-    quantity = request.json['quantity']
+from controllers.product_controller import products
 
-    new_product = Product(name, description, price, quantity)
-
-    db.session.add(new_product)
-    db.session.commit()
-
-    return product_schema.jsonify(new_product)
-
-@app.route('/products', methods=['GET'])
-def get_products():
-    all_products = Product.query.all()
-    return products_schema.jsonify(all_products)
-
-@app.route('/products/<id>', methods=['GET'])
-def get_product(id):
-    product = Product.query.get(id)
-    return product_schema.jsonify(product)
-
-@app.route('/products/<id>', methods=['PUT'])
-def update_product(id):
-    product = Product.query.get(id)
-
-    name = request.json['name']
-    description = request.json['description']
-    price = request.json['price']
-    quantity = request.json['quantity']
-
-    product.name = name
-    product.description = description
-    product.price = price
-    product.quantity = quantity
-
-    db.session.commit()
-
-    return product_schema.jsonify(product)
-
-@app.route('/products/<id>', methods=['DELETE'])
-def delete_product(id):
-    product = Product.query.get(id)
-    db.session.delete(product)
-    db.session.commit()
-
-    return product_schema.jsonify(product)
+app.register_blueprint(products, url_prefix='/products')
 
 @app.route('/', methods =['GET'])
 def get():
---
+++ controllers/product_controller.py
@@ -0,0 +1,58 @@
+from flask import Blueprint, request, jsonify
+
+from db import db
+from models.product import Product
+from schemas.productSchema import ProductSchema, product_schema, products_schema
+
+products = Blueprint('products', __name__)
+
+@products.route('/', methods=['POST'])
+def add_product():
+    name = request.json['name']
+    description = request.json['description']
+    price = request.json['price']
+    quantity = request.json['quantity']
+
+    new_product = Product(name, description, price, quantity)
+
+    db.session.add(new_product)
+    db.session.commit()
+
+    return product_schema.jsonify(new_product)
+
+@products.route('/', methods=['GET'])
+def get_products():
+    all_products = Product.query.all()
+    print(all_products)
+    return products_schema.jsonify(all_products)
+
+@products.route('/<id>', methods=['GET'])
+def get_product(id):
+    product = Product.query.get(id)
+    return product_schema.jsonify(product)
+
+@products.route('/<id>', methods=['PUT'])
+def update_product(id):
+    product = Product.query.get(id)
+
+    name = request.json['name']
+    description = request.json['description']
+    price = request.json['price']
+    quantity = request.json['quantity']
+
+    product.name = name
+    product.description = description
+    product.price = price
+    product.quantity = quantity
+
+    db.session.commit()
+
+    return product_schema.jsonify(product)
+
+@products.route('/<id>', methods=['DELETE'])
+def delete_product(id):
+    product = Product.query.get(id)
+    db.session.delete(product)
+    db.session.commit()
+
+    return product_schema.jsonify(product)
